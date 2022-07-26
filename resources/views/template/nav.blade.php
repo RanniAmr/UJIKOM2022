f200b62f1da669373316d7463d8abd828da07b80
RanniAmr <ranniamran06@gmail.com>
2022-07-26 12:18:32 +0700

    ujikom 2022 ranni

---
+++ resources/views/template/nav.blade.php
@@ -0,0 +1,17 @@
+<ul class="nav">
+  <li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="#">Run</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Home</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Produk</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link disabled">Keranjang</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link disabled">Checkout</a>
+  </li>
+</ul>
